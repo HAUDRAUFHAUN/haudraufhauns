f7c0bb317783ba0576fffa801a9703f78485fcb3
Dietrich <dietrich.poensgen@heg-portal.de>
2021-02-10 14:56:41 +0100

    fix frontmatter

--- src/pages/blog/index.svelte
+++ src/pages/blog/index.svelte
@@ -7,16 +7,19 @@
 
 {#each posts as node}
   <div
-    class="p-8 min-w-full md:py-10 mx-2 my-2 md:my-6 shadow-lg rounded-md prose dark:text-white"
+    class="p-8 min-w-full md:py-10 mx-2 my-2 md:my-6 shadow-lg rounded-md dark:text-white dark:bg-gray-900"
   >
-    <h2>
+    <h2 class="text-xl md:text-2xl lg:text-3xl ">
       <a
-        class="p-2 rounded-md dark:text-white hover:bg-green-200 hover:text-green-700 dark:hover:bg-gray-700 dark:hover:text-green-500"
+        class="rounded-md p-2 border-transparent border-dashed border-b-2 hover:border-green-400 hover:bg-green-200 hover:text-green-700 dark:hover:bg-gray-700 dark:hover:text-green-500"
         href={$url(node.path)}
       >
-        {node.title.toUpperCase()}
+        {node.meta.frontmatter.title}
       </a>
     </h2>
-    <p class="text-gray-600 dark:text-gray-50">Some description</p>
+    <br />
+    <p class="text-gray-600 dark:text-gray-50">
+      {node.meta.frontmatter.description}
+    </p>
   </div>
 {/each}
