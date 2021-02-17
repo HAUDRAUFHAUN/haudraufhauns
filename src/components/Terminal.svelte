11ec73f7eb028bbee23fe544755d82fc203c5503
Dietrich <dietrich.poensgen@heg-portal.de>
2021-02-17 13:21:01 +0100

    update terminal

--- src/components/Terminal.svelte
+++ src/components/Terminal.svelte
@@ -242,7 +242,39 @@
       }
       100% {
         opacity: 1;
-        width: 20%;
+        width: 100%;
+      }
+    }
+  }
+
+  @screen lg {
+    @keyframes typing {
+      0% {
+        width: 0%;
+        border-right: 0.15em solid white;
+      }
+      40% {
+        width: 0%;
+      }
+      100% {
+        width: 30%;
+      }
+    }
+    @keyframes typing-2 {
+      0% {
+        width: 0%;
+        border-right: 0.15em solid white;
+      }
+      1% {
+        opacity: 1;
+      }
+      40% {
+        width: 0%;
+        border-right: 0.15em solid white;
+      }
+      100% {
+        opacity: 1;
+        width: 100%;
       }
     }
   }
