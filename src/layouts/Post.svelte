021e89087923e2ec1dbd4b26fcd199b3fa43efa8
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2021-03-28 21:40:55 +0200

    finish first version of blogpost

--- src/layouts/Post.svelte
+++ src/layouts/Post.svelte
@@ -3,17 +3,18 @@
   export let title,
     description = "";
 
-  $: metatags.title = title + " Post";
+  $: metatags.title = title;
   $: metatags.description = description;
 </script>
 
-<h1 class="text-xl md:text-2xl lg:text-4xl xl:text-5xl 2xl:text-6xl mb-6 pb-2 font-syne font-semibold">
-  {title}
-</h1>
-<div
-  class="prose md:prose-lg lg:prose-xl dark:text-white  max-w-xs sm:max-w-md md:max-w-3xl lg:max-w-5xl"
-  id="post-content"
->
-  <slot />
-</div>
-
+  <h1
+    class="text-xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl mb-6 pb-2 font-syne font-semibold"
+  >
+    {title}
+  </h1>
+  <div
+    class="prose md:prose-lg lg:prose-xl dark:text-white max-w-xs sm:max-w-md md:max-w-3xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl"
+    id="post-content"
+  >
+    <slot />
+  </div>
