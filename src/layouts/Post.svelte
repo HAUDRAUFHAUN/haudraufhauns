9b48f3209f8ca00731948ce7ab21650a849ff0a6
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2021-04-02 21:23:18 +0200

    adjust width of blogpost content

--- src/layouts/Post.svelte
+++ src/layouts/Post.svelte
@@ -7,18 +7,20 @@
   $: metatags.description = description;
 </script>
 
-<div id="content">
-  <h1
-    class="text-xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl mb-6 pb-2 font-syne font-semibold"
-  >
-    {title}
-  </h1>
-  <div
-    class="prose-sm md:prose lg:prose-lg dark:text-white max-w-xs sm:max-w-md md:max-w-3xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-5xl"
-    id="post-content"
-  >
-    <slot />
-  </div>
+<div id="content" class="grid grid-cols-1 place-items-center">
+  <article class="w-full md:w-5/6 lg:w-4/6 2xl:w-1/2">
+    <h1
+      class="text-left text-xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl mb-6 pb-2 font-syne font-semibold text-gray-900"
+    >
+      {title}
+    </h1>
+    <div
+      class="prose-sm md:prose lg:prose-lg dark:text-white max-w-xs sm:max-w-md md:max-w-3xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-5xl"
+      id="post-content"
+    >
+      <slot />
+    </div>
+  </article>
 </div>
 
 <style>
